2156e660b6fe2bfec7f179a727731a099cd04152
alex_zen <zensanches@mail.ru>
2022-06-24 23:00:49 +0300

    удалил лишнюю строку

--- lesson7/task_7_1_hw.py
+++ lesson7/task_7_1_hw.py
@@ -11,7 +11,6 @@ import os
 main_dir = 'my_project'
 if not os.path.exists(main_dir):
     os.mkdir(main_dir)
-
 os.chdir(main_dir) #перешли в папку my_project
 other_paths = ['settings', 'mainapp', 'adminapp', 'authapp']
 for i in range(len(other_paths)):
